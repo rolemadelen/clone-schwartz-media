982b7f2c6e99263cff085f499aaba3fce29d90b2
Madelen Yoo <rolemadelen@tuta.io>
2020-08-22 14:39:45 +0900

    main section header & 'View Our Company' button styled
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- src/main.js
+++ src/main.js
@@ -1,2 +1,21 @@
 import '../styles/style.scss';
 'use strict';
+
+const imgSlideshow =  document.querySelector('img#image-slideshow');
+const images = ['carousel_tm_1.jpg', 'carousel_tm_2.jpg', 'carousel_tm_3.jpg']
+const imgPath = "./assets/images/";
+let imgPosition = 0;
+
+function imageSlideshow() {
+    setInterval(() => {
+        if (imgPosition >= images.length) {
+            imgPosition = 0;
+        }
+    
+        imgSlideshow.src = imgPath + images[imgPosition];
+        ++imgPosition;
+        console.log(imgPosition);
+    }, 3500);
+}
+
+// document.onload = imageSlideshow();
